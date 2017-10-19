55f751ffa4715c89d32c0d1a7f1612e4c744fb7e
angelxvalle <31350154+angelxvalle@users.noreply.github.com>
2017-10-19 13:47:08 -0400

    tuition.cpp

---
+++ tuition.cpp
@@ -0,0 +1,34 @@
+/*tuition.cpp
+Angel Valle
+October 19, 2017
+This program calculates the increase in tuition cost over a five year period.
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+		//Declare and initialize variables and constants
+	const float RATE = 0.04;
+	const float INITIAL_COST = 115.0;
+	float costPerCredit = 0.0;
+			
+		//Display chart heading
+	cout << "Year\t\tCost per Credit\n-----------------------------\n";
+	costPerCredit = INITIAL_COST;
+		for (int year = 2008; year <= 2013; year++)
+		{
+			cout << fixed << setprecision(2);
+			cout << year << "\t\t$" << costPerCredit << endl; //display year and costPerCredit
+				//Calculate costPerCredit with increase
+			costPerCredit = costPerCredit + costPerCredit * RATE;
+		}
+
+
+
+
+	return 0;
+}
